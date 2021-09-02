4b5d87ee35459e5ef8bfe7d8a0772fb7cef2fad9
Tamino Huxohl <thuxohl@techfak.uni-bielefeld.de>
2021-09-02 22:07:23 +0200

    include arm64 image in manifest generation

--- generate-manifest.sh
+++ generate-manifest.sh
@@ -1,6 +1,8 @@
-docker manifest create openbaseorg/spread:experimental openbaseorg/spread:experimental-amd64 openbaseorg/spread:experimental-arm32
+docker manifest create openbaseorg/spread:experimental openbaseorg/spread:experimental-amd64 openbaseorg/spread:experimental-arm32 openbaseorg/spread:experimental-arm64
 docker manifest annotate openbaseorg/spread:experimental openbaseorg/spread:experimental-arm32 --arch arm
+docker manifest annotate openbaseorg/spread:experimental openbaseorg/spread:experimental-arm64 --arch arm64
 docker manifest push --purge openbaseorg/spread:experimental
-docker manifest create openbaseorg/spread:latest openbaseorg/spread:latest-amd64 openbaseorg/spread:latest-arm32
+docker manifest create openbaseorg/spread:latest openbaseorg/spread:latest-amd64 openbaseorg/spread:latest-arm32 openbaseorg/spread:latest-arm64
 docker manifest annotate openbaseorg/spread:latest openbaseorg/spread:latest-arm32 --arch arm
+docker manifest annotate openbaseorg/spread:latest openbaseorg/spread:latest-arm64 --arch arm64
 docker manifest push --purge openbaseorg/spread:latest
